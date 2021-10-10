1f015ba10821625fd61b2123b8345a01660b5150
themisbo <43866785+themisbo@users.noreply.github.com>
2021-10-10 21:36:27 +0100

    Update app.py

--- deployment/app.py
+++ deployment/app.py
@@ -12,10 +12,7 @@ penguins = penguins.drop("year", axis = 1)
 penguins = penguins.drop("island", axis = 1)
 penguins = penguins.drop("sex", axis = 1)
 
-# Dictionary containing the mapping
 variety_mappings = {0: 'Adelie', 1: 'Gentoo', 2: 'Chinstrap'}
-
-# Encoding the target variables to integers
 penguins = penguins.replace(['Adelie', 'Gentoo' , 'Chinstrap'],[0, 1, 2])
 
 species = penguins.pop('species')
@@ -25,26 +22,10 @@ pipe_svc = Pipeline([ ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=F
 pipe_svc.fit(penguins, species)
 
 def classify(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g):
-    arr = np.array([bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g]) # Convert to numpy array
+    arr = np.array([bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g])
     arr = arr.astype(np.float64)
     query = arr.reshape(1, -1)
 
     prediction = variety_mappings[pipe_svc.predict(query)[0]]
 
     return prediction
-
-# def classify(island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex):
-#     arr = pd.DataFrame([island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex])
-#     query = arr.to_numpy().reshape(1, -1)
-#     prediction = pipe_svc.predict(query) 
-
-#     prediction = variety_mappings[pipe_svc.predict(query)[0]]
-
-#     return prediction
-
-# island ="Biscoe"
-# bill_length_mm = 48
-# bill_depth_mm = 16
-# flipper_length_mm = 220
-# body_mass_g = 5400
-# sex = "male"
